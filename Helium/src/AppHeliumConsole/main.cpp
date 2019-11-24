299915cf3b74d28bc390f84a49985eb774e8bed5
Mickey <dunyazad@hanmail.net>
2019-11-25 08:54:36 +0900

    .

--- Helium/src/AppHeliumConsole/main.cpp
+++ Helium/src/AppHeliumConsole/main.cpp
@@ -10,7 +10,7 @@ void main()
 
 	auto he = HELIUM;
 	auto window1 = he->CreateHeliumWindow(nullptr);
-	//auto window2 = he->CreateHeliumWindow(nullptr);
+	auto window2 = he->CreateHeliumWindow(nullptr);
 
 	window1->CreateScene("window1");
 	//window2->CreateScene("window2");
